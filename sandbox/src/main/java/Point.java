8a224c29a8b4ceb8b56b50325608f41ae354364f
Shunika <nastashu@gmail.com>
2016-08-18 00:54:58 +0300

    Первая часть задания

--- sandbox/src/main/java/Point.java
+++ sandbox/src/main/java/Point.java
@@ -4,6 +4,14 @@ package ru.stqa.sandbox;
  * Created by shunya on 18.08.2016.
  */
 public class Point {
-  double a;
-  double b;
+  double p1;
+  double p2;
+  double p3;
+  double p4;
+ public Point  (double p1, double p2, double p3, double p4) {
+  this.p1=p1;
+  this.p2=p2;
+  this.p3 = p3;
+  this.p4 = p4;
+ }
 }
--- sandbox/src/main/java/hello_world.java
+++ sandbox/src/main/java/hello_world.java
@@ -12,12 +12,17 @@ public class hello_world {
 
         Rectangle r= new Rectangle(4,6);
 
+        Point d = new Point (2,1,3,2);
+
         System.out.println( "Площадь квадрата со стороной " + s.l+ "="+ area(s) );
+
         System.out.println(" Площадь прямоугольника  со сторонами" + r.a + "и" + r.b +"=" + area(r));
+        System.out.println(" Расстояние"+"=" + distance(d));
     }
         public static void hello (String somebody)
         {
             System.out.println("Hello,"+ somebody + "!");}
+
     public static double area (Square s )
     {return s.l*s.l;
     }
@@ -25,7 +30,10 @@ public class hello_world {
     public static double area (Rectangle r){
     return r.a*r.b;
 }
-    public static double distance(Point p1, Point p2)
+    public static double distance(Point d)
+    {return Math.sqrt((d.p1-d.p2)*(d.p1-d.p2) +(d.p3-d.p4)*(d.p3-d.p4));
+    }
+
 }
 
 
