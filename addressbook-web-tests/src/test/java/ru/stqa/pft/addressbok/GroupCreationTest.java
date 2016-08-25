04b56c7faac9891dce077161dc710dd89280d15a
Shunika <nastashu@gmail.com>
2016-08-25 23:36:11 +0300

    Фикстура кода
    Переименован метод, перенесен код из @Test в @BeforeMethod

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbok/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbok/GroupCreationTest.java
@@ -17,22 +17,36 @@ import static org.openqa.selenium.OutputType.*;
 public class GroupCreationTest {
     FirefoxDriver wd;
     
+    public static boolean isAlertPresent(FirefoxDriver wd) {
+        try {
+            wd.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+
     @BeforeMethod
     public void setUp() throws Exception {
         wd = new FirefoxDriver();
         wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
         wd.get("http://localhost/addressbook/");
+
+        login();
+    }
+
+    private void login() {
         wd.findElement(By.name("user")).click();
         wd.findElement(By.name("user")).clear();
         wd.findElement(By.name("user")).sendKeys("admin");
         wd.findElement(By.name("pass")).click();
         wd.findElement(By.name("pass")).clear();
         wd.findElement(By.name("pass")).sendKeys("secret");
-        wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
+        wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click(); //вынесено из метода @Test
     }
     
     @Test
-    public void testGroupCreation() {
+    public void testGroupCreation() {//method
 
         wd.findElement(By.linkText("groups")).click();
         wd.findElement(By.name("new")).click();
@@ -52,14 +66,5 @@ public class GroupCreationTest {
     @AfterMethod
     public void tearDown() {
         wd.quit();
-    }
-    
-    public static boolean isAlertPresent(FirefoxDriver wd) {
-        try {
-            wd.switchTo().alert();
-            return true;
-        } catch (NoAlertPresentException e) {
-            return false;
-        }
     }
 }
